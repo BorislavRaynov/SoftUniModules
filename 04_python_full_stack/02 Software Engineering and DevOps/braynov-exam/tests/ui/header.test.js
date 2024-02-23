d10ec6992c7608047dbe170ca4a71c4a51f73d39
Borislav Raynov <b_raynov@abv.bg>
2024-02-23 16:59:59 +0200

    Exercise added.

---
+++ 04_python_full_stack/02 Software Engineering and DevOps/braynov-exam/tests/ui/header.test.js
@@ -0,0 +1,9 @@
+const { test, expect } = require('@playwright/test');
+
+test('Check header', async ({ page }) => {
+    await page.goto('https://braynov-exam.onrender.com/'); 
+    const homeLink = await page.$('[href="/"]');
+    const text = await homeLink.textContent();
+    expect(text).toBe('Home');
+  });
+  
