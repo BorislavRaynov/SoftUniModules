40a53f13c4f4b670bfaa432654d226fb4b051f74
BorislavRaynov <b_raynov@abv.bg>
2024-06-29 09:52:23 +0300

    Footer component added.

--- 04_python_full_stack/03 ReactJS/01.ReactJS-Intro-to-React-and-JSX/html-template/src/App.jsx
+++ 04_python_full_stack/03 ReactJS/01.ReactJS-Intro-to-React-and-JSX/html-template/src/App.jsx
@@ -8,6 +8,7 @@ import Testimonials from "./Components/Testimonials"
 import FAQ from "./Components/FAQ"
 import ContactUs from "./Components/ContactUs"
 import DownloadApp from "./Components/DownloadApp"
+import Footer from "./Components/Footer"
 
 function App() {
   return (
@@ -32,15 +33,7 @@ function App() {
 
         <DownloadApp />
 
-        <footer className="footer-copy">
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-md-12">
-                <p>2018 &copy; Applight. Website Designed by <a href="http://w3Template.com" target="_blank" rel="dofollow">W3 Template</a></p>
-              </div>
-            </div>
-          </div>
-        </footer>
+        <Footer />
     </>
   )
 }
