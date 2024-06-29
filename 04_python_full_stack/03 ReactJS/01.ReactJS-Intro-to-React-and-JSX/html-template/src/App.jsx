91b100071707254350400dfb7bd3d708069870e0
BorislavRaynov <b_raynov@abv.bg>
2024-06-29 09:50:17 +0300

    DownloadApp component added.

--- 04_python_full_stack/03 ReactJS/01.ReactJS-Intro-to-React-and-JSX/html-template/src/App.jsx
+++ 04_python_full_stack/03 ReactJS/01.ReactJS-Intro-to-React-and-JSX/html-template/src/App.jsx
@@ -7,6 +7,7 @@ import OurTeam from "./Components/OurTeam"
 import Testimonials from "./Components/Testimonials"
 import FAQ from "./Components/FAQ"
 import ContactUs from "./Components/ContactUs"
+import DownloadApp from "./Components/DownloadApp"
 
 function App() {
   return (
@@ -29,27 +30,7 @@ function App() {
 
         <ContactUs />
 
-        <section className="download section-padding">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="sectioner-header text-center white">
-                  <h3>Download Our App</h3>
-                  <span className="line"></span>
-                  <p className="white">Sed quis nisi nisi. Proin consectetur porttitor dui sit amet viverra. Fusce sit amet lorem faucibus, vestibulum ante in, pharetra ante.</p>
-                </div>
-              </div>
-              <div className="col-md-12">
-                <div className="section-content text-center">
-                  <ul>
-                    <li><a href="#"><img src="images/appstore.png" className="wow fadeInUp" data-wow-delay="0.4s"/></a></li>
-                    <li><a href="#"><img src="images/playstore.png" className="wow fadeInUp" data-wow-delay="0.7s"/></a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <DownloadApp />
 
         <footer className="footer-copy">
           <div className="container-fluid">
