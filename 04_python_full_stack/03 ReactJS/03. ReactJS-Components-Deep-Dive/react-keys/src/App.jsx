a503b754a75ff015c0de68c11e30c3fe16d51087
BorislavRaynov <b_raynov@abv.bg>
2024-07-06 06:32:59 +0300

    Exercise added.

---
+++ 04_python_full_stack/03 ReactJS/03. ReactJS-Components-Deep-Dive/react-keys/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+	const [movies, setMovies] = useState([
+		'The Matrix',
+		'Man of Steel',
+		'Lord of t Rings',
+	]);
+
+	const btnClickHandler = () => {
+		setMovies((oldMovies) => []);
+	}
+
+	return (
+		<>
+			<h1>Movies</h1>
+
+			<ul>
+				<li>{movies[0]}</li>
+				<li>{movies[1]}</li>
+				<li>{movies[2]}</li>
+			</ul>
+
+			<button onClick={btnClickHandler}>Change</button>
+		</>
+	)
+}
+
+export default App
