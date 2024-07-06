008cb96b24cc88999fbfb4f22a662e64690b604a
BorislavRaynov <b_raynov@abv.bg>
2024-07-06 07:31:10 +0300

    useEffect added.

--- 04_python_full_stack/03 ReactJS/03. ReactJS-Components-Deep-Dive/react-timer/src/components/Timer.jsx
+++ 04_python_full_stack/03 ReactJS/03. ReactJS-Components-Deep-Dive/react-timer/src/components/Timer.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Timer() {
-    const [time, setTime] = useState(0)
+    const [time, setTime] = useState(() => {
+        const currentSeconds = new Date().getSeconds();
 
-    setTimeout(() => {
-        setTime(prevTime => prevTime + 1)
-    }, 1000);
+        return currentSeconds;
+    });
+
+    useEffect(() => {
+        setTimeout(() => {
+            setTime(prevTime => prevTime + 1)
+        }, 1000);
+    }, []);
+
+    const addSecondsHandler = () => {
+        setTime(prevTime => prevTime + 10);
+    }
 
     return (
         <>
             <h1>Timer</h1>
             <div>{time}</div>
+            <button onClick={addSecondsHandler}>Add Seconds</button>
         </>
     )
 }
